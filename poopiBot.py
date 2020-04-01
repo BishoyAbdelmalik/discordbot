6bfffc6d6628ea71f79504253dc75654f25aaa70
mrboushy <mrboushy@users.noreply.github.com>
2020-04-01 15:35:48 +0000

    first commit

---
+++ poopiBot.py
@@ -0,0 +1,42 @@
+# poopiBot.py
+import discord
+import threading
+import asyncio
+
+TOKEN = open("token.txt").read()
+
+client = discord.Client()
+# gwhy=True
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if 'gWhy' in message.content:
+        gwhy=True
+        await message.channel.send('CANBAS')
+        # while True:
+            # await message.channel.send('CANBAS')
+            # await asyncio.sleep(10)
+            # await asyncio.sleep(10)
+            # if not gwhy:
+            #     return
+        return
+    if 'CANBAS'.lower() in message.content.lower():
+        #print("stop canbas")
+        # gwhy=False
+        await message.channel.send('gWhy')
+        return
+    if 'why' in message.content.lower():
+        await message.channel.send('Gwacause')
+        return
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, poopi welcoms you!'
+    )
+client.run(TOKEN)
