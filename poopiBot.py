522f737cff0e4be2a61f969ecf5433b46b020a56
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-06-22 11:18:37 -0700

    change one of the memes sources

--- poopiBot.py
+++ poopiBot.py
@@ -18,7 +18,7 @@ meme = "http://localhost:956/moderate"
 meme2 = "http://localhost:956/sbubby"
 meme3 = "http://localhost:956/dank"
 meme4 = "http://localhost:956/light"
-meme5 = "http://localhost:956/hmm"
+meme5 = "http://localhost:956/programing"
 meme6 = "http://localhost:956/meme"
 print(os.system("node /bot/memeAPI/server.js &"))
 emojiThumbsUp = '\N{THUMBS UP SIGN}'
