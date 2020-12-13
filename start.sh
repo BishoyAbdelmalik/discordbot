27e0132671ea0ae6f2de858be682abcde96acbfb
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-12-13 14:36:20 -0800

    edit start script

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ python3 -m pip install requests
 python3 -m pip install FFmpeg  
 python3 -m pip install youtube_dl
 python3 -m pip install ffprobe 
-python3 -m pip install json 
 python3 -m pip install validators 
 python3 -m pip install youtube-search 
 python3 poopiBot.py
